841a071e369dcc8b63c682035aa6ceec05c4b2aa
LOGESH SD <148134953+Logesh-18-logesh@users.noreply.github.com>
2024-06-27 18:36:56 +0530

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict([data['features']])
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
